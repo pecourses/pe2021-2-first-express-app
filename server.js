a4f03c979fa4febc9e83468042e51946d9eae69d
Viktoriia <pecourses>
2022-05-22 17:27:12 +0300

    Add server

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.get('/', (req, res) => {
   res.status(200).end();
 });
 
+app.post('/', (req, res) => {});
+app.patch('/', (req, res) => {});
+app.delete('/', (req, res) => {});
+
+app.get('/contests', (req, res) => {});
+app.post('/contests', (req, res) => {});
+app.patch('/contests/1', (req, res) => {});
+app.delete('/contests/1', (req, res) => {});
+
 const PORT = process.env.PORT || 5000;
 
 const httpServer = http.createServer(app);
