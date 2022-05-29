ba6b4217e9a0aec0325ced181a03e760e0f0c9be
Viktoriia <pecourses>
2022-05-29 23:06:38 +0300

    Refactor validation handlers to async/await syntax

--- middleware/validate.js
+++ middleware/validate.js
@@ -3,27 +3,31 @@ const {
   CONTACT_UPDATING_VALIDATION_SCHEMA,
 } = require("../utils/validationSchemas");
 
-module.exports.validateContactOnCreate = (req, res, next) => {
+module.exports.validateContactOnCreate = async (req, res, next) => {
   const { body } = req;
 
-  CONTACT_CREATION_VALIDATION_SCHEMA.validate(body)
-    .then((validatedContact) => {
-      req.body = validatedContact;
-      next();
-    })
-    .catch((err) => {
-      res.status(422).send(err);
-    });
+  try {
+    const validatedContact = await CONTACT_CREATION_VALIDATION_SCHEMA.validate(
+      body
+    );
+    req.body = validatedContact;
+    next();
+  } catch (err) {
+    next(err);
+    // res.status(422).send(err);
+  }
 };
 
-module.exports.validateContactOnUpdate = (req, res, next) => {
+module.exports.validateContactOnUpdate = async (req, res, next) => {
   const { body } = req;
-  CONTACT_UPDATING_VALIDATION_SCHEMA.validate(body)
-    .then((validatedUpdatedContact) => {
-      req.body = validatedUpdatedContact;
-      next();
-    })
-    .catch((err) => {
-      res.status(422).send(err);
-    });
+
+  try {
+    const validatedUpdateContact =
+      await CONTACT_UPDATING_VALIDATION_SCHEMA.validate(body);
+    req.body = validatedUpdateContact;
+    next();
+  } catch (err) {
+    next(err);
+    // res.status(422).send(err);
+  }
 };
